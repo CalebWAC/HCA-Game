a3b96f68499ca2a2cc9e6dfe1a0a5092182f023c
Caleb Borlin <calebwac@outlook.com>
2024-06-28 12:20:52 -0600

    vertical blocks generate properly

--- FSharpScripts/WorldGenerator.fs
+++ FSharpScripts/WorldGenerator.fs
@@ -12,10 +12,11 @@ module WorldFS =
     let mutable self = Unchecked.defaultof<Node3D>
     
     let world = [|
-        block -5f 0f -5f; block -5f 0f -3f; block -5f 0f 3f; block -5f 0f 5f
-        block -3f 0f -5f; block -3f 0f -3f; block -3f 0f 3f; block -3f 0f 5f
-        block 3f 0f -5f; block 3f 0f -3f; block 3f 0f 3f; block 3f 0f 5f
-        block 5f 0f -5f; block 5f 0f -3f; block 5f 0f 3f; block 5f 0f 5f
+        block -2f 0f -2f; block -2f 1f -1f; block -2f 2f 0f; block -2f 3f 1f; block -2f 3f 2f 
+        block -1f 0f -2f; block -1f 1f -1f; block -1f 1f 0f; block -1f 1f 1f; block -1f 2f 2f
+        block 0f 0f -2f; block 0f 0f -1f; block 0f 0f 0f; block 0f 0f 1f; block 0f 0f 2f
+        block 1f 0f -2f; block 1f 0f -1f; block 1f 0f 0f; block 1f 1f 1f; block 1f 1f 2f
+        block 2f 0f -2f; block 2f 0f -1f; block 2f 1f 0f; block 2f 1f 1f; block 2f 2f 2f
     |]
     
     let ready () =
@@ -28,10 +29,10 @@ module WorldFS =
         
         world
         |> Array.iter (fun data ->
-            let block = new CsgBox3D()
-            block.Position <- data.position
-            sceneTree.Root.GetNode("Node3D").GetNode<Node3D>("WorldGenerator").AddChild(block)
-            GD.Print $"{block.Position}"
+            for i in 0f .. data.position.Y do
+                let block = new CsgBox3D()
+                block.Position <- Vector3(data.position.X, i, data.position.Z)
+                sceneTree.Root.GetNode("Node3D").GetNode<Node3D>("WorldGenerator").AddChild(block)
         )
         
     let process delta =
