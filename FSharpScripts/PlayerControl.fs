4d8fe86cc5605c8900139f598ff82f22e3dce125
Caleb Borlin <calebwac@outlook.com>
2024-06-29 10:55:55 -0600

    movement boundaries added

--- FSharpScripts/PlayerControl.fs
+++ FSharpScripts/PlayerControl.fs
@@ -4,6 +4,8 @@ open Godot
 open GlobalFunctions
 
 module PlayerControlFS =
+    type Direction = Left | Right | Forward | Backward
+    
     let mutable left = Unchecked.defaultof<Button>
     let mutable right = Unchecked.defaultof<Button>
     let mutable forward = Unchecked.defaultof<Button>
@@ -12,10 +14,21 @@ module PlayerControlFS =
     let mutable rotateR = Unchecked.defaultof<Button>
     let mutable player = Unchecked.defaultof<Node3D>
 
-    let moveLeft () = player.Translate(Vector3(1f, 0f, 0f))
-    let moveRight () = player.Translate(Vector3(-1f, 0f, 0f))
-    let moveForward () = player.Translate(Vector3(0f, 0f, 1f))
-    let moveBackward () = player.Translate(Vector3(0f, 0f, -1f))
+    let withinBoundaries direction =
+           let dir = 
+               match direction with
+               | Forward -> player.Transform.Basis.Z
+               | Backward -> -player.Transform.Basis.Z
+               | Left -> player.Transform.Basis.X
+               | Right -> -player.Transform.Basis.X
+           
+           (dir + player.Position).Z < 5f && (dir + player.Position).Z > -5f &&
+           (dir+ player.Position).X < 5f && (dir + player.Position).X > -5f
+    
+    let moveLeft () = if withinBoundaries Left then player.Translate(Vector3(1f, 0f, 0f))
+    let moveRight () = if withinBoundaries Right then player.Translate(Vector3(-1f, 0f, 0f))
+    let moveForward () = if withinBoundaries Forward then player.Translate(Vector3(0f, 0f, 1f))
+    let moveBackward () = if withinBoundaries Backward then player.Translate(Vector3(0f, 0f, -1f))
     let rotateLeft () = degToRad 90f |> player.RotateY
     let rotateRight () = degToRad -90f |> player.RotateY
     
