26ce44e0b1b2d521ee9ff14e72169750a4dd8f8f
Caleb Borlin <calebwac@outlook.com>
2024-07-01 17:19:50 -0600

    collision with goal functional

--- FSharpScripts/Goal.fs
+++ FSharpScripts/Goal.fs
@@ -4,4 +4,8 @@ open Godot
 open GlobalFunctions
 
 module GoalFS =
-    ()
+    let onAreaEntered () = getRoot().GetTree().ChangeSceneToFile("res://LevelSelect.tscn") |> ignore
+    
+    let ready () =
+        let area3d = getRoot().GetNode<Node3D>("WorldGenerator").GetNode<Node3D>("Goal").GetNode<Area3D>("Area3D")
+        area3d.add_AreaEntered (fun x -> onAreaEntered())
--- Scripts/Goal.cs
+++ Scripts/Goal.cs
@@ -1,20 +1,16 @@
 using Godot;
-using System;
+using FSharpScripts;
 
 public partial class Goal : Node3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		GoalFS.ready();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	}
-
-	public void OnAreaEntered()
-	{
-		GD.Print("You have reached the goal!");
 	}
 }
