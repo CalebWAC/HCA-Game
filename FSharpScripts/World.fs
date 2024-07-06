754369f4570f0e2aac8979f7385f71be7865f80c
Caleb Borlin <calebwac@outlook.com>
2024-07-06 10:08:26 -0600

    world setup for third level

--- FSharpScripts/World.fs
+++ FSharpScripts/World.fs
@@ -66,6 +66,21 @@ module WorldFS =
            block 5f 0f -6f; block 5f 2f -5f; block 5f 2f -4f; block 5f 1f -3f; block 5f 0f -2f; block 5f 0f -1f; block 5f 0f 0f; block 5f 0f 1f; block 5f 1f 2f; block 5f 0f 3f; block 5f 0f 4f; block 5f 0f 5f; block 5f 1f 6f
            block 6f 0f -6f; block 6f 0f -5f; block 6f 0f -4f; block 6f 0f -3f; block 6f 0f -2f; block 6f 0f -1f; block 6f 0f 0f; block 6f 0f 1f; block 6f 1f 2f; block 6f 0f 3f; block 6f 0f 4f; block 6f 1f 5f; block 6f 2f 6f
         |]
+        [| // Level 3
+           block -6f 5f -6f; block -6f 5f -5f; block -6f 5f -4f; waterBlock -6f 0f -3f; block -6f 0f -2f; block -6f 5f -1f; block -6f 4f 0f; waterBlock -6f 0f 1f; waterBlock -6f 0f 2f; waterBlock -6f 0f 3f; waterBlock -6f 0f 4f; waterBlock -6f 0f 5f; waterBlock -6f 0f 6f
+           block -5f 5f -6f; block -5f 6f -5f; block -5f 5f -4f; waterBlock -5f 0f -3f; block -5f 1f -2f; block -5f 6f -1f; block -5f 3f 0f; waterBlock -5f 0f 1f; block -5f 5f 2f; block -5f 5f 3f; block -5f 5f 4f; waterBlock -5f 0f 5f; waterBlock -5f 0f 6f
+           block -4f 5f -6f; block -4f 5f -5f; block -4f 5f -4f; waterBlock -4f 0f -3f; block -4f 1f -2f; block -4f 1f -1f; block -4f 2f 0f; waterBlock -4f 0f 1f; waterBlock -4f 0f 2f; waterBlock -4f 0f 3f; waterBlock -4f 0f 4f; waterBlock -4f 0f 5f; waterBlock -4f 0f 6f
+           waterBlock -3f 0f -6f; waterBlock -3f 0f -5f; waterBlock -3f 0f -4f; waterBlock -3f 0f -3f; block -3f 0f -2f; block -3f 1f -1f; waterBlock -3f 0f 0f; waterBlock -3f 0f 1f; waterBlock -3f 0f 2f; waterBlock -3f 0f 3f; waterBlock -3f 0f 4f; block -3f 5f 5f; waterBlock -3f 0f 6f
+           waterBlock -2f 0f -6f; waterBlock -2f 0f -5f; waterBlock -2f 0f -4f; waterBlock -2f 0f -3f; block -2f 0f -2f; block -2f 0f -1f; waterBlock -2f 0f 0f; waterBlock -2f 0f 1f; waterBlock -2f 0f 2f; waterBlock -2f 0f 3f; waterBlock -2f 0f 4f; block -2f 5f 5f; waterBlock -2f 0f 6f
+           waterBlock -1f 0f -6f; block -1f 3f -5f; block -1f 3f -4f; waterBlock -1f 0f -3f; block -1f 0f -2f; waterBlock -1f 0f -1f; waterBlock -1f 0f 0f; waterBlock -1f 0f 1f; block -1f 5f 2f; waterBlock -1f 0f 3f; waterBlock -1f 0f 4f; waterBlock -1f 0f 5f; waterBlock -1f 0f 6f
+           waterBlock 0f 0f -6f; block 0f 2f -5f; block 0f 1f -4f; waterBlock 1f 0f -3f; block 0f 0f -2f; waterBlock 0f 0f -1f; waterBlock 0f 0f 0f; waterBlock 0f 0f 1f; block 0f 5f 2f; waterBlock 0f 0f 3f; waterBlock 0f 0f 4f; waterBlock 0f 0f 5f; waterBlock 0f 0f 6f
+           waterBlock 1f 0f -6f; waterBlock 1f 0f -5f; waterBlock 1f 0f -4f; waterBlock 1f 0f -3f; waterBlock 1f 0f -2f; waterBlock 1f 0f -1f; waterBlock 1f 0f 0f; waterBlock 1f 0f 1f; block 1f 5f 2f; waterBlock 1f 0f 3f; waterBlock 1f 0f 4f; block 1f 5f 5f; waterBlock 1f 0f 6f
+           block 2f 5f -6f; block 2f 5f -5f; block 2f 5f -4f; block 2f 5f -3f; block 2f 5f -2f; block 2f 5f -1f; block 2f 5f 0f; waterBlock 2f 0f 1f; block 2f 5f 2f; waterBlock 2f 0f 3f; waterBlock 2f 0f 4f; block 2f 5f 5f; waterBlock 2f 0f 6f
+           block 3f 0f -6f; block 3f 0f -5f; block 3f 0f -4f; block 3f 0f -3f; waterBlock 3f 0f -2f; waterBlock 3f 0f -1f; waterBlock 3f 0f 0f; waterBlock 3f 0f 1f; waterBlock 3f 0f 2f; waterBlock 3f 0f 3f; waterBlock 3f 0f 4f; block 3f 5f 5f; waterBlock 3f 0f 6f
+           block 4f 0f -6f; block 4f 0f -5f; block 4f 0f -4f; block 4f 0f -3f; waterBlock 4f 0f -2f; waterBlock 4f 0f -1f; waterBlock 4f 0f 0f; waterBlock 4f 0f 1f; waterBlock 4f 0f 2f; waterBlock 4f 0f 3f; waterBlock 4f 0f 4f; block 4f 5f 5f; waterBlock 4f 0f 6f
+           block 5f 0f -6f; block 5f 0f -5f; block 5f 0f -4f; block 5f 0f -3f; waterBlock 5f 0f -2f; waterBlock 5f 0f -1f; waterBlock 5f 0f 0f; waterBlock 5f 0f 1f; waterBlock 5f 0f 2f; waterBlock 5f 0f 3f; waterBlock 5f 0f 4f; block 5f 5f 5f; waterBlock 5f 0f 6f
+           block 6f 0f -6f; block 6f 0f -5f; block 6f 0f -4f; block 6f 5f -3f; block 6f 5f -2f; block 6f 5f -1f; block 6f 5f 0f; block 6f 5f 1f; block 6f 5f 2f; block 6f 5f 3f; waterBlock 6f 0f 4f; waterBlock 6f 0f 5f; waterBlock 6f 0f 6f
+        |]
     |]
     
     let elements = [|
@@ -77,9 +92,16 @@ module WorldFS =
             { etype = Hook; position = Vector3(-3f, 7f, -5.5f); rotation = Vector3.Zero }
         |]
         [| // Level 2
-            { etype = Goal; position = Vector3(-3f, 8f, 3f); rotation = Vector3.Zero }
+            
             { etype = Bridge; position = Vector3(-5f, 1f, -3.5f); rotation = Vector3(0f, degToRad 90f, 0f) }
-            { etype = LavaWall; position = Vector3(3f, 1.5f, 2f); rotation = Vector3(0f, degToRad 90f, 0f) }
+        |]
+        [| // Level 3
+            { etype = Goal; position = Vector3(-5f, 7f, -5f); rotation = Vector3.Zero }
+            { etype = LavaWall; position = Vector3(6f, 5.5f, 0f); rotation = Vector3.Zero }
+            { etype = Hook; position = Vector3(2.5f, 5f, -4f); rotation = Vector3(0f, degToRad 90f, 0f) }
+            { etype = Hook; position = Vector3(5.5f, 5f, 0f); rotation = Vector3(0f, degToRad 90f, 0f) }
+            { etype = Hook; position = Vector3(3f, 5f, 4.5f); rotation = Vector3.Zero }
+            { etype = Hook; position = Vector3(0f, 5f, 2.5f); rotation = Vector3.Zero }
         |]
     |]
     
@@ -89,6 +111,9 @@ module WorldFS =
         |]
         [| // Level 2
             { ptype = TerrainManipulator; position = Vector3(4f, 7f, -4f) }
+        |]
+        [| // Level 3
+            { ptype = GrapplingHook; position = Vector3(4f, 1f, -5f) }
         |]
     |]
     
--- Scripts/Hook.cs
+++ Scripts/Hook.cs
@@ -6,7 +6,7 @@ public partial class Hook : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		HookFS.ready();
+		HookFS.ready(this);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
